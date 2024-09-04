bb1fd0ce174f2fe345fbf7353bd1dc98cca2d32c
davidnjoroge <ndegwa.david@outlook.com>
2024-09-04 11:24:06 +0300

    updates

--- maishajunction/build.sh
+++ maishajunction/build.sh
@@ -2,21 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Upgrade pip
-pip install --upgrade pip
-
-# Install AWS CLI (if needed)
-pip install awscli
-
-# Upgrade setuptools and wheel
-pip install --upgrade setuptools wheel
-pip install ez_setup
-
-# Install requirements
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Collect static files (if applicable for Django projects)
+# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply database migrations (if applicable for Django projects)
+# Apply any outstanding database migrations
 python manage.py migrate
--- maishajunction/maishajunction/settings.py
+++ maishajunction/maishajunction/settings.py
@@ -89,16 +89,15 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+"""
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://maishajunction_user:owPofPCZ8TTT16dE4qjgE4nN0T3XyiE7@dpg-crbn4rrqf0us73deq8d0-a.oregon-postgres.render.com/maishajunction',
+        default='postgresql://postgres:postgres@localhost:5432/maishajunction',
         conn_max_age=600
     )
 }
-"""
 
 
 
